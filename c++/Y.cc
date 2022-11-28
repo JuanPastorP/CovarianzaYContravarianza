8978527ebc0a937185c198aaf3b9dd0f4202a4c0
JuanPastor <jpastorp@unsa.edu.pe>
2022-11-27 23:17:59 -0500

    Se permite covarianza en el retorno de C

--- c++/Y.cc
+++ c++/Y.cc
@@ -1,7 +1,14 @@
 #include "Y.h"
 #include "objetoA.h"
 #include "objetoC.h"
-B* Y::foo(B* test){
+/*B* Y::foo(B* test){
     test->fooB();
-    return test;
+    objetoB* prueba = new objetoB();
+    return prueba;
+}*/
+C* Y::foo(B* test){    
+    objetoC* prueba = new objetoC();
+    prueba->fooC();
+    return prueba;
 }
+// Si permite covarianza de al retornar C*
--- c++/Y.h
+++ c++/Y.h
@@ -1,8 +1,11 @@
 #ifndef Y_H
 #define Y_H
 #include "X.h"
+#include "objetoC.h"
+#include "objetoA.h"
 class Y: public X{
     public:
-        B* foo(B* test);
+        //B* foo(B* test) override;
+        C* foo(B* test) override;
 };
 #endif
