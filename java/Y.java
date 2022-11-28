45e2a8cd68b1061849d3a4153f5516575f392386
JuanPastor <jpastorp@unsa.edu.pe>
2022-11-27 19:33:54 -0500

    Primer intento de contravarianza tratando de que retorne A en lugar de B no funciona

--- java/Y.java
+++ java/Y.java
@@ -12,3 +12,10 @@ public class Y extends X{
 		return new objetoA();
 		}
     }
+/* No lo permite error:
+./Y.java:10: error: foo(B) in Y cannot override foo(B) in X
+public A foo(B test){
+         ^
+return type A is not compatible with B
+1 error
+*/
